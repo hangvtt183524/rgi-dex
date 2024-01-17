7ca80e4dd9d22cdf74d81a2f92b2ea5be1c3db6c
hang.nguyen <111733892+hangvu0420@users.noreply.github.com>
2024-01-17 12:14:47 +0700

    check installed false

--- src/components/WalletModal/WalletModal.tsx
+++ src/components/WalletModal/WalletModal.tsx
@@ -99,7 +99,7 @@ const WalletModal = <T,>({ login, onDismiss = () => null, displayCount = 5, wall
                 onDismiss={onDismiss}
                 disabled={!confirmed}
               />
-              <Text>{wallet?.installed ? 'true' : 'false'} - {wallet?.href}</Text>
+              <Text>{wallet.installed === false ? 'false' : 'true'} - {wallet?.href}</Text>
             </>
           ))}
           {/* {!showMore && sortedConfig.length > 4 && <MoreWalletCard onClick={() => setShowMore(true)} />} */}
